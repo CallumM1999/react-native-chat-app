429ca15406ec56b374e005c1df3fb8b62b893ff0
cal <callummac@protonmail.com>
2019-01-22 13:34:21 +0000

    Simplified Unread reducer

--- app/reducers/unread.js
+++ app/reducers/unread.js
@@ -5,16 +5,12 @@ const defaultState = () => ({
 const unread = (state = defaultState(), action) => {
 	switch (action.type) {
 	case 'ADD_MESSAGE':
+
 		return {
 			...state,
-			[action.room]: state.hasOwnProperty(action.room) ? [
-				...state[action.room],
-				action.message
-			] : [
-				action.message
-			]
-
+			[action.room]: true
 		};
+
 	case 'OPEN_ROOM':
 		return Object.keys(state).filter(room => room !== action.room);
 
